9f30600ad9241b018af0cc3e4e017360277d168d
ShermanOneberry <107899491+ShermanOneberry@users.noreply.github.com>
2023-03-15 11:48:24 +0800

    Potential secure registry setup

--- scratch/configure.sh
+++ scratch/configure.sh
@@ -9,3 +9,15 @@ if [ ! -f "certs/domain.key" ] && [ ! -f "certs/domain.crt"]; then
 else
     echo "Cert and key already generated"
 fi
+
+mkdir -p nginx_certs
+if [ ! -f "nginx_certs/domain.key" ] && [ ! -f "nginx_certs/domain.crt"]; then
+    echo "Creating keys-cert pair"
+    openssl req \
+    -newkey rsa:4096 -nodes -sha256 -keyout nginx_certs/server.key \
+    -addext "subjectAltName = DNS:192.168.56.102" \
+    -x509 -days 365 -out nginx_certs/server.crt \
+    -subj "/C=SG/ST=Singapore/L=Singapore/O=Oneberry/CN=registry"
+else
+    echo "Cert and key already generated"
+fi
